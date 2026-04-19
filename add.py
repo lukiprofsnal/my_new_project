b925d6d6ebfbc13b56fc48e3edd6abcc238b3c0f
lukiprofsnal <lukiprofsnal@gmail.com>
2026-04-19 12:08:28 +0530

    Modify add function to accept parameters and user input

--- add.py
+++ add.py
@@ -1,7 +1,9 @@
-def add():
+def add(a,b):
     c=a+b
     return c
 
-return c
+a=eval(input('enter the value of a :',a))
+b=eval(input('enter the value of b :',b))
+print('the addition of two values: ',add(a,b))
 
 
